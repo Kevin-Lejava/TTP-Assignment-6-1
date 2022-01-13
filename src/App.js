46a4b7d55ec3d144173ffd0eac05c5c28b78a881
Kevin-Lejava <klejava@gmail.com>
2022-01-12 19:39:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [zipcode, setZipcode] = useState("");
+  const [data, setData] = useState([]);
+  console.log(zipcode);
+
+  const getCities = (event, zipcode) => {
+    event.preventDefault();
+    fetch(`http://ctp-zip-api.herokuapp.com/zip/${zipcode}`)
+      .then((response) => {
+        console.log(response)
+        return response.json();
+      }).then((data) => {
+        console.log(data)
+        setData(data);
+      }).catch((err) => {
+        console.error(err)
+      })
+  }
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Fragment>
+          <form>
+            <label>
+              Zipcode:
+              <input type="text" name="zipcode" onChange={(e) => setZipcode(e.target.value)} />
+            </label>
+            <input type="submit" value="Submit" onClick={(e) => getCities(e, zipcode)} />
+          </form>
+
+        </Fragment>
+
+        {data.map((element) => {
+          return (
+
+            <div className="card">
+              <h3>{element.City}</h3>
+              <ul>
+                <li className="list-group-item">State: {element.State}</li>
+                <li className="list-group-item">Location: {element.Lat}, {element.Long}</li>
+                <li className="list-group-item">Population(esitmated): {element.EstimatedPopulation}</li>
+                <li className="list-group-item">Total Wages: {element.TotalWages}</li>
+              </ul>
+              <br></br>
+            </div>
+
+          )
+        })}
+
       </header>
+
+
+
     </div>
   );
 }
